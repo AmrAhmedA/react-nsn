554f185cb4c299b8f3f7c02e962fa3cae5cca38e
AmrAhmedA <Geek3mr@gmail.com>
2023-03-07 00:16:41 +0200

    feat: add open close logic

--- src/OnlineStatusNotifier.tsx
+++ src/OnlineStatusNotifier.tsx
@@ -1,17 +1,52 @@
+import React, { useEffect, useState } from 'react';
 import { cancelIcon, offlineIcon, onlineIcon } from './icons';
 
 type OnlineStatusNotifierType = {
   isOnline: boolean;
+  duration?: number;
 };
-export function OnlineStatusNotifier({ isOnline }: OnlineStatusNotifierType) {
+export function OnlineStatusNotifier({
+  isOnline,
+  duration = 4.5
+}: OnlineStatusNotifierType) {
+  const [hovering, setHovering] = React.useState(false);
+
+  const [isOpen, setIsOpen] = useState(true);
+
   const handleCloseButtonClick = () => {};
 
   const handleRefreshButtonClick = () =>
     // eslint-disable-next-line no-restricted-globals
     location.reload();
 
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (!hovering && duration > 0) {
+      const timeout = setTimeout(() => {
+        onClose();
+      }, duration * 1000);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [duration, hovering]);
+
+  if (!isOpen) return null;
+
   return (
-    <div className="statusNotifierAnchorOriginBottomLeft">
+    <div
+      className="statusNotifierAnchorOriginBottomLeft"
+      onMouseEnter={() => {
+        setHovering(true);
+      }}
+      onMouseLeave={() => {
+        setHovering(false);
+      }}
+    >
       <div>{isOnline ? onlineIcon : offlineIcon}</div>
       <div>{getStatusText(isOnline)}</div>
       {!isOnline && (
