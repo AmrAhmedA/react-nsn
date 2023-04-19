211753691a2380b64f171012538e15fc2c48c0a0
AmrAhmedA <Geek3mr@gmail.com>
2023-04-19 22:11:30 +0200

    fix: css plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/nsn.ts'),
