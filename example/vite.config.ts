346ce391b6757edafcb2196fe0739c3b2c76805e
AmrAhmedA <Geek3mr@gmail.com>
2023-04-24 23:02:34 +0200

    chore: base url

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://amrahmeda.github.io/react-nsn/'
 })
