df3f25a261202cfcbdae6eed55f66c0d4df68b57
AmrAhmedA <Geek3mr@gmail.com>
2023-04-25 00:19:41 +0200

    chore: build script for prod

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,8 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://amrahmeda.github.io/react-nsn/'
+  plugins: [react()]
 })
