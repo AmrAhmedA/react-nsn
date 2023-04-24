644c3b8c58664d98a113533be6a17e37d197b7fb
AmrAhmedA <Geek3mr@gmail.com>
2023-04-24 16:50:12 +0200

    feat: info button

--- example/src/components/AppBar.tsx
+++ example/src/components/AppBar.tsx
@@ -1,9 +1,10 @@
 import GitHubIcon from '@mui/icons-material/GitHub'
+import InfoIcon from '@mui/icons-material/Info'
+import { Tooltip } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import IconButton from '@mui/material/IconButton'
 import Toolbar from '@mui/material/Toolbar'
-import Tooltip from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
 
 export function AppBarContainer() {
@@ -14,6 +15,16 @@ export function AppBarContainer() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             React Network status notification - React-nsn
           </Typography>
+          <Tooltip title="show me how to disable my network">
+            <IconButton
+              size="large"
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+            >
+              <InfoIcon />
+            </IconButton>
+          </Tooltip>
           <Tooltip title="React-nsn git repository" arrow>
             <IconButton
               size="large"
