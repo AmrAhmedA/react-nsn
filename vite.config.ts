fa3d7d724ce87128ff82304b76dd776e8d15204a
AmrAhmedA <Geek3mr@gmail.com>
2023-07-22 22:11:26 +0300

    chore: optimize production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
 import * as packageJson from './package.json'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { PluginOption, defineConfig } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({ jsxRuntime: 'classic' }),
     cssInjectedByJsPlugin(),
     dts({
       insertTypesEntry: true
-    })
+    }),
+    visualizer({
+      template: 'treemap', // or sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'analyse.html' // will be saved in project's root
+    }) as PluginOption
   ],
+
   build: {
     lib: {
       entry: resolve(__dirname, 'src/nsn.ts'),
